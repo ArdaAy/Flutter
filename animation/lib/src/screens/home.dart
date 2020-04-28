4c40c897303fbc30d99b360ed818a5689cc5db49
Arda AY <arda.ay@outlook.com.tr>
2020-04-29 02:12:05 +0300

    RX Dart Example and Animation Start
    RxDart eğitim projesi eklendi. Ayrıca yeni eğitim konusu olan animasyonlara başlandı.

---
+++ animation/lib/src/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  
+  State<StatefulWidget> createState() {
+    
+    return null;
+  }
+
+}
+
+class HomeState extends State<Home>{
+
+}
--- login_bloc_rxdart/lib/src/screens/login_screen.dart
+++ login_bloc_rxdart/lib/src/screens/login_screen.dart
@@ -13,7 +13,7 @@ class LoginScreen extends StatelessWidget {
           emailField(bloc),
           passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
-          submitButton(),
+          submitButton(bloc),
         ],
       ),
     );
@@ -54,11 +54,21 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      child: Text('login'),
-      color: Colors.blue,
-      onPressed: () {},
+  Widget submitButton(Bloc bloc) {
+
+    return StreamBuilder(
+      stream: bloc.submitValid,  
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('login'),
+          color: Colors.blue,
+          onPressed: snapshot.hasData
+            ? () {
+              bloc.submit();
+            }
+            : null,
+        );
+      },
     );
   }
 }
