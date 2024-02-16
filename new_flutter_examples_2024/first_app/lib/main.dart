1cf4002629b19526722c32b57e5f504058ffab70
Arda AY <arda.ay@outlook.com.tr>
2024-02-16 08:38:12 +0300

    MaterialApp with Scaffold and Centered Text Widget (auto-formatted code)

--- new_flutter_examples_2024/first_app/lib/main.dart
+++ new_flutter_examples_2024/first_app/lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
 }
