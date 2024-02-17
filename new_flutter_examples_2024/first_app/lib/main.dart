14d3864002af6746bba544451a09d3e810416389
Arda AY <arda.ay@outlook.com.tr>
2024-02-17 07:33:18 +0300

    Scaffold backgroundColor

--- new_flutter_examples_2024/first_app/lib/main.dart
+++ new_flutter_examples_2024/first_app/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 135, 88, 216),
         body: Center(
           child: Text('Hello World!'),
         ),
