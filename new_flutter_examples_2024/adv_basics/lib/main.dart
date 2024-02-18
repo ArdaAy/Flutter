f0825d75309e556ba05ca22b825ce2a75bcac85d
Arda AY <arda.ay@outlook.com.tr>
2024-02-18 21:41:14 +0300

    Challange Development for adv_basics

--- new_flutter_examples_2024/adv_basics/lib/main.dart
+++ new_flutter_examples_2024/adv_basics/lib/main.dart
@@ -3,7 +3,48 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.yellow, Colors.deepPurple],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  height: 300,
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: LinearBorder(),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Start Quiz'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
