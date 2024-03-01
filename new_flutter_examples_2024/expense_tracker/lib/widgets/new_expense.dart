2a4c9493a42093476d40ee3af361a495ece26656
Arda AY <arda.ay@outlook.com.tr>
2024-03-01 07:30:01 +0300

    Validating User Input & Showing an Error Dialog

--- new_flutter_examples_2024/expense_tracker/lib/widgets/new_expense.dart
+++ new_flutter_examples_2024/expense_tracker/lib/widgets/new_expense.dart
@@ -30,6 +30,39 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      // show error message
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date and category was entered'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+      return;
+    }
+
+    debugPrint(_titleController.text);
+    debugPrint(_amountController.text);
+    //print(_titleController.text);
+    //print(_amountController.text);
+    //print(_enteredTitle);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -90,9 +123,7 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
-          SizedBox(
-            height: 16,
-          ),
+          const SizedBox(height: 16),
           Row(
             children: [
               DropdownButton(
@@ -118,11 +149,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text("Cancel"),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                  //print(_enteredTitle);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
