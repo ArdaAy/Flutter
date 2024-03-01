b7bfe0f612b23357d8e5856f3fbb2337a343c173
Arda AY <arda.ay@outlook.com.tr>
2024-03-01 07:58:42 +0300

    Using the Dismissible Widget for Dismissing List Items

--- new_flutter_examples_2024/expense_tracker/lib/widgets/expenses.dart
+++ new_flutter_examples_2024/expense_tracker/lib/widgets/expenses.dart
@@ -41,6 +41,12 @@ class _ExpenseState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +62,11 @@ class _ExpenseState extends State<Expenses> {
       body: Column(
         children: [
           const Text('The Chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+              child: ExpensesList(
+            expenses: _registeredExpenses,
+            onRemoveExpense: _removeExpense,
+          )),
         ],
       ),
     );
