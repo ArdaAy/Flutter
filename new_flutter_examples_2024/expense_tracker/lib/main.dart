5cd0ba6a5afdece1366bf811fab5bd4246cc6312
Arda AY <arda.ay@outlook.com.tr>
2024-03-02 09:24:28 +0300

    More Theming

--- new_flutter_examples_2024/expense_tracker/lib/main.dart
+++ new_flutter_examples_2024/expense_tracker/lib/main.dart
@@ -2,17 +2,37 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 96, 59, 181),
+  seedColor: Color.fromARGB(255, 49, 48, 50),
 );
 
 void main(List<String> args) {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer)),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
       home: Expenses(),
     ),
   );
