9a987e5efac51faf4f0c43e314fc5f721301aca3
Arda AY <arda.ay@outlook.com.tr>
2024-03-12 18:16:09 +0300

    Managing App-wide State & Data

--- new_flutter_examples_2024/meals/lib/screens/tabs.dart
+++ new_flutter_examples_2024/meals/lib/screens/tabs.dart
@@ -16,12 +16,28 @@ class _TabsScreenState extends State<TabsScreen> {
   int _SelectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favorite.');
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showInfoMessage('Marked as a favorite!');
     }
   }
 
@@ -40,7 +56,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_SelectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = 'Your Favorites';
