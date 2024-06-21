bc67e714c50828d45da1a16fc1ad10e7f07b827e
Arda AY <arda.ay@outlook.com.tr>
2024-06-22 00:52:29 +0300

    Favorite Places Challenge Personal Solution

--- new_flutter_examples_2024/favorite_places/lib/widgets/places_list.dart
+++ new_flutter_examples_2024/favorite_places/lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place_item.dart';
+import 'package:favorite_places/widgets/new_place.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,11 +12,34 @@ class PlacesList extends StatefulWidget {
 
 class _PlacesListState extends State<PlacesList> {
   final List<PlaceItem> _placesList = [];
+
+  void _addNewPlace(PlaceItem placeItem) {
+    setState(() {
+      _placesList.add(placeItem);
+    });
+    for (var i = 0; i < _placesList.length; i++) {
+      print(_placesList[i].title);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => NewPlace(
+                        addNewPlace: _addNewPlace,
+                      ),
+                    ));
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       body: ListView.builder(
         padding: const EdgeInsets.all(8),
