59c15e541f613822eaf35d6f552350b869751cc8
Arda AY <arda.ay@outlook.com.tr>
2024-06-23 10:51:08 +0300

    Storing the Location Data in the Model

--- new_flutter_examples_2024/favorite_places/lib/widgets/location_input.dart
+++ new_flutter_examples_2024/favorite_places/lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -14,7 +15,7 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
   void _getCurrentLocation() async {
@@ -48,6 +49,10 @@ class _LocationInputState extends State<LocationInput> {
     final lat = _locationData.latitude;
     final lng = _locationData.longitude;
 
+    if (lat == null || lng == null) {
+      return;
+    }
+
     final url = Uri.parse(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyAwiEHBp3Lz2ql8-7OKQNnDMAropjvRxxk');
     final response = await http.get(url);
@@ -55,6 +60,8 @@ class _LocationInputState extends State<LocationInput> {
     final address = resData['results'][0]['formatted_address'];
 
     setState(() {
+      _pickedLocation =
+          PlaceLocation(latitude: lat!, longitude: lng!, address: address);
       _isGettingLocation = false;
     });
 
