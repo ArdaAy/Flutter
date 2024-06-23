7c655cd5b076d31d86437aeb9c36221647524fcf
Arda AY <arda.ay@outlook.com.tr>
2024-06-24 00:38:27 +0300

    Installing & Configuring the Google Maps Package

--- new_flutter_examples_2024/favorite_places/ios/Runner/AppDelegate.swift
+++ new_flutter_examples_2024/favorite_places/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAwiEHBp3Lz2ql8-7OKQNnDMAropjvRxxk")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
